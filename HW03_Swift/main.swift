8e13d51572f23cf42af58253f258afb2b998232b
Omid-SH <hdomid@yahoo.com>
2020-06-15 08:15:55 -0700

    Initial Commit

---
+++ HW03_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HW03_Swift
+//
+//  Created by Omid on 6/15/20.
+//  Copyright © 2020 MobileProgramming. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
